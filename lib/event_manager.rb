d22909c022b73d2223f9eabc982976a5a7a67482
Bobby Luly <bobbyluly@gmail.com>
2022-12-28 17:05:07 -0700

    Finish through It2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,35 @@
 # ./lib/event_manager.rb
 require "csv"
+
+# def clean_zipcode(zipcode)
+#   if zipcode.nil?
+#     "00000"
+#   elsif zipcode.length < 5
+#     zipcode.rjust(5,"0")
+#   elsif zipcode.length > 5
+#     zipcode[0..4]
+#   else
+#     zipcode
+#   end
+# end
+
+#clean_zipcode Refactor
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
 puts "EventManager initialized."
+
+contents = CSV.open "./data/event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
+
+
+# if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
